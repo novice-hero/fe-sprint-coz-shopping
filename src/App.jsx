621b0486a89e940cac0fddecaa7fc78fc7664001
novice-hero <highero700@gmail.com>
2023-05-12 22:32:59 +0900

    feat: 메인페이지, 상품리스트페이지, 북마크페이지 생성 후 App에 라우터 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { Route, Routes } from "react-router";
+import Header from "./components/ui/Header";
+import MainPage from "./pages/MainPage";
+import ProductListPage from "./pages/ProductListPage";
+import BookmarkListPage from "./pages/BookmarkListPage";
+import Footer from "./components/ui/Footer";
+
 function App() {
-  return <>{"hi"}</>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/product/list" element={<ProductListPage />} />
+        <Route path="/bookmark" element={<BookmarkListPage />} />
+        <Route path="*" element={<MainPage />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
