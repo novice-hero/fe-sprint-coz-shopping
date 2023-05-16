680fc63a53e6c67aec683bbda5c47e999875ff49
novice-hero <highero700@gmail.com>
2023-05-16 21:57:32 +0900

    feat: 모든 상품 정보를 불러오는 api 추가

--- src/api/cozShoppingApi.js
+++ src/api/cozShoppingApi.js
@@ -6,4 +6,12 @@ export default {
     const data = await response.json();
     return data;
   },
+
+  async getAllItem() {
+    const response = await fetch(
+      "http://cozshopping.codestates-seb.link/api/v1/products"
+    );
+    const data = await response.json();
+    return data;
+  },
 };
